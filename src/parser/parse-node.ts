a2757fcf417151f02dce1a0ce4b17d6e0355987d
고윤호 <yoonho.go@bbros.kr>
2021-06-22 15:51:05 +0900

    refactor: type nodes를 별도 파일로 분리하여 parse

--- src/parser/parse-node.ts
+++ src/parser/parse-node.ts
@@ -1,19 +1,15 @@
 import * as ts from 'typescript';
-import parseArrayTypeNode from './parse-array-type-node';
 import parseDeclaration from './parse-declaration';
 import parseEnumMember from './parse-enum-member';
 import parseIdentifier from './parse-identifier';
-import parseLiteralType from './parse-literal-type';
 import parsePropertySignature from './parse-property-signature';
 import parseSyntaxList from './parse-syntax-list';
 import parseToken from './parse-token';
-import parseTupleTypeNode from './parse-tuple-type-node';
 import parseTypeElement from './parse-type-element';
 import parseTypeLiteralNode from './parse-type-literal-node';
 import parseTypeParameter from './parse-type-parameter';
 import parseTypeReferenceNode from './parse-type-reference-node';
-import parseUnionType from './parse-union-type';
-import parseIntersectionType from './parse-intersection-type';
+import parseTypeNode from './parse-type-node';
 
 export default (node?: ts.Node) => {
   if (!node) {
@@ -40,10 +36,6 @@ export default (node?: ts.Node) => {
     return parseEnumMember(node);
   }
 
-  if (ts.isArrayTypeNode(node)) {
-    return parseArrayTypeNode(node);
-  }
-
   if (ts.isTypeReferenceNode(node)) {
     return parseTypeReferenceNode(node);
   }
@@ -52,24 +44,12 @@ export default (node?: ts.Node) => {
     return parseTypeLiteralNode(node);
   }
 
-  if (ts.isTupleTypeNode(node)) {
-    return parseTupleTypeNode(node);
-  }
-
-  if (ts.isUnionTypeNode(node)) {
-    return parseUnionType(node);
-  }
-
-  if (ts.isLiteralTypeNode(node)) {
-    return parseLiteralType(node);
-  }
-
   if (ts.isTypeParameterDeclaration(node)) {
     return parseTypeParameter(node);
   }
 
-  if (ts.isIntersectionTypeNode(node)) {
-    return parseIntersectionType(node);
+  if (ts.isTypeNode(node)) {
+    return parseTypeNode(node);
   }
 
   if (node.kind === ts.SyntaxKind.SyntaxList) {
---
+++ src/parser/parse-type-node.ts
@@ -0,0 +1,28 @@
+import ts = require("typescript");
+import parseArrayTypeNode from "./parse-array-type-node";
+import parseIntersectionType from "./parse-intersection-type";
+import parseLiteralType from "./parse-literal-type";
+import parseTupleTypeNode from "./parse-tuple-type-node";
+import parseUnionType from "./parse-union-type";
+
+export default (node: ts.TypeNode) => {
+  if (ts.isArrayTypeNode(node)) {
+    return parseArrayTypeNode(node);
+  }
+
+  if (ts.isTupleTypeNode(node)) {
+    return parseTupleTypeNode(node);
+  }
+
+  if (ts.isUnionTypeNode(node)) {
+    return parseUnionType(node);
+  }
+
+  if (ts.isLiteralTypeNode(node)) {
+    return parseLiteralType(node);
+  }
+
+  if (ts.isIntersectionTypeNode(node)) {
+    return parseIntersectionType(node);
+  }
+}
